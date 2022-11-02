b3a54efb52a9b690843e37971e89a14700407381
Thabo Sodi <thabosodi11@gmail.com>
2022-11-02 11:39:53 +0200

    parse data

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 async function getNotes() {
     const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30');
     const data = await res.json();
@@ -13,9 +15,22 @@ export default async function NotesPage() {
             <div>
             {notes?.map((notes) =>{
                 return <Note key={note._id} note={note} />;
-            }))}
+            })}
 
             </div>
         </div>
     )
 }
+
+function Note ({note}: any) {
+    const {id, title, content, created} = note || {};
+    return (
+        <Link href={`/notes/${id}`}>
+            <div>
+                <h2>{title}</h2>
+                <h5>{content}</h5>
+                <p>{created}</p>
+            </div>
+        </Link>
+    );
+})
