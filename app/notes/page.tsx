3dd4cf4d01b6a549eba95ac49dcefebe281e04a0
Thabo Sodi <thabosodi11@gmail.com>
2022-11-02 11:51:42 +0200

    cache

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,6 +4,7 @@ async function getNotes() {
     const res = await fetch(
         'http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30',
         {cache: 'no-store'}
+        // this is like getServerSideProps 
         );
     const data = await res.json();
     return data?.items as any[];
