a51e5e2adec8c16f1777549462d3284ddd9f86b1
Thabo Sodi <thabosodi11@gmail.com>
2022-11-02 11:25:34 +0200

    convert notes to json

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,6 +1,6 @@
 async function getNotes() {
     const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30');
-    
+    const data = await res.json();
 }
 
 export default async function NotesPage(){
